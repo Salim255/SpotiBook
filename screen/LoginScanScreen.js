f066a061453c216922fa27b6931b19599e9a8d52
Salim <salim.hassan.welleat@gmail.com>
2023-05-17 14:11:36 +0200

    Add: SafeAreaView

--- screen/LoginScanScreen.js
+++ screen/LoginScanScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import Button from "../components/UI/Button";
 function LoginScanScreen() {
   return (
